f07c45d488feabe9d452b0d2aa6422028520858f
artom-madarij <madarijartom@gmail.com>
2024-09-10 13:42:50 +0200

    Add files via upload

---
+++ food.py
@@ -0,0 +1,21 @@
+## Лабораторна робота з дисципліни "Алгоритмізація та програмування"
+## Виконав: Мадярій Артьом Іванович(ІР-14)
+## Лабораторна робота №1 (Варіант 12)
+#створимо змінну
+food = 'Млинці'
+print(food)
+
+#створимо змінні
+is_adult = True
+is_child = False
+print(is_adult, is_child)
+
+is_snowing = False
+is_raining = True
+print(not is_snowing, not is_raining)
+
+#iмпортуємо математичну бібіотеку, виконуємо математичний вираз
+import math
+x = 0.067
+y = 3.017
+print(137*x**3+math.cos(y**3/x**4)+math.tan(14*y)-7*x**6)
