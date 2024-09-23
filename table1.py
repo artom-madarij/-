e0707969b2e6fbe7a394f6f3c5001ee296d7b14d
artom-madarij <madarijartom@gmail.com>
2024-09-23 17:40:32 +0300

    table1.py

--- table1.py
+++ table1.py
@@ -1,16 +1,26 @@
+## Лабораторна робота з дисципліни "Алгоритмізація та програмування"
+## Виконав: Мадярій Артьом Іванович(ІР-14)
+## Лабораторна робота №2 (Варіант 12)
 import math as m
+
 a = 0.5
 b = 2.0
 h = 0.2
+
+
 def arctan(x):
     return 1 / m.tan(x)
+
+
 def function(x):
     if x < 1:
-        return m.cos(m.sqrt(x**3))
+        return m.cos(m.sqrt(x ** 3))
     elif 1 <= x <= 1.5:
         return arctan(m.exp(x))
     else:
-        return m.sin(m.log(x))**5
+        return m.sin(m.log(x)) ** 5
+
+
 x = a
 print("Табулювання функції на проміжку [0.5, 2.0] з кроком 0.2:")
 print("x\t\t\tfunction(x)\n")
